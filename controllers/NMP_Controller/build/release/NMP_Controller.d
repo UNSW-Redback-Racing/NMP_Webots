410130bff50744d276f6037d4bf5162ba6b5853b
Drumil Sevak <z5371484@ad.unsw.edu.au>
2021-03-19 23:10:00 +1100

    Converted Code to C

---
+++ controllers/NMP_Controller/build/release/NMP_Controller.d
@@ -0,0 +1,12 @@
+build/release/NMP_Controller.o: NMP_Controller.c \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/lidar.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/lidar_point.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/accelerometer.h \
+ C:\Users\drumi\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h
